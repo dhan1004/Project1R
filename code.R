f9d85a0e65df96999229abd5e3ba5f435b33b61e
Dixi Han <sissyhan1008@gmail.com>
2023-05-25 23:07:22 -0400

    ahhhhhhhh pls

--- code.R
+++ code.R
@@ -1,1 +1,16 @@
-2_2
+data <- read.csv("femaleMiceWeights.csv")
+ncol(data)
+name(data[,2])
+colnames(data)
+data[12,2]
+weight <- data$Bodyweight
+weight[11]
+length(data)
+length(weight)
+data
+
+# calculate mean weight of mice on hf diet
+mean(weight[13:24])
+
+set.seed(1)
+sample(weight[13:24], 1)
